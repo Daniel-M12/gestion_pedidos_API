643fa51c0780eabbaba346d31b577a46b363249c
Daniel Matos <deme12.1@gmail.com>
2023-05-05 07:08:53 -0500

    agregado proyecto y archivos para autenticacion por token

---
+++ restaurante.DBEntity/Base/EntityBaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DBEntity
+{
+	public class EntityBaseResponse
+	{
+		public bool isSuccess { get; set; }
+
+		public string errorCode { get; set; }
+
+		public string errorMessage { get; set; }
+
+		public object data { get; set; }
+	}
+}
+
